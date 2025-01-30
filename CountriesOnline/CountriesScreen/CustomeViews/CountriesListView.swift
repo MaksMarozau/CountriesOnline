05e835c87d002f4e82757603cd6c9fdb5fbf7c03
Maks Marozau <licwin11@yandex.ru>
2025-01-30 23:12:22 +0300

    Add additional functionality
    Added filter and sort. Implemented errors processing with alert. CountriesViewModel was refactored.

---
+++ CountriesOnline/CountriesScreen/CustomeViews/CountriesListView.swift
@@ -0,0 +1,18 @@
+//
+//  CountriesListView.swift
+//  CountriesOnline
+//
+//  Created by Maks on 30.01.25.
+//
+
+import SwiftUI
+
+struct CountriesListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CountriesListView()
+}
